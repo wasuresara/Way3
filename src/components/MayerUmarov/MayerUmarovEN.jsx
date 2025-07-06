edf1e0a2313ca03ae0dad5ed9e0c4b17775cb513
Kopputr <145269523+wasuresara@users.noreply.github.com>
2025-07-07 02:20:28 +0300

    refactor(MayerUmarovEN): simplify component structure and add GIF for visual enhancement
    - Remove modal functionality to streamline the component.
    - Introduce a GIF image to enhance visual appeal and engagement.
    - Update layout for improved readability and consistency.

--- src/components/MayerUmarov/MayerUmarovEN.jsx
+++ src/components/MayerUmarov/MayerUmarovEN.jsx
@@ -1,53 +1,19 @@
 import React, { useState } from "react";
 import s from "../Prices/Prices.module.css";
+import MayerUmarovGif from "../About/gif1.gif";
 
 function MayerUmarovEN() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
-    <div id="clothing" className={s.my}>
-      {!isModalOpen && (
-        <div>
-          <h2 className={s.sectionTitle}>MAYER. UMAROV</h2>
-          <p className={s.sectionOverview}>
-            Upcycled clothing. <br /> We give products a new life - we
-            reconstruct secondary materials into a unique object. <br />
-            Each thing is unique.
-          </p>
-          <button onClick={openModal} className={s.button}>
-            Learn More
-          </button>
-        </div>
-      )}
-      {isModalOpen && (
-        <div>
-          <h2 className={s.sectionTitle}>MAYER. UMAROV</h2>
-          <p className={s.sectionOverview}>
-            Upcycled clothing. <br /> We give products a new life - we
-            reconstruct secondary materials into a unique object. <br />
-            Each thing is unique.
-          </p>
-          <button onClick={closeModal} className={s.button}>
-            Close
-          </button>
-          <div className={s.contentSection}>
-            <a
-              href="https://www.instagram.com/mayer.umarov/"
-              target="_blank"
-              className={s.linkButton}
-            >
-              Instagram
-            </a>
-          </div>
-        </div>
-      )}
+    <div id="clothing" className={s.container}>
+      <div>
+        <h2 className={s.sectionTitle}>MAYER. UMAROV</h2>
+        <img src={MayerUmarovGif} alt="MayerUmarovGif" className={s.PricesGif}/>
+        <p className={s.sectionOverview}>
+          Upcycled clothing. <br /> We give products a new life - we
+          reconstruct secondary materials into a unique object. <br />
+          Each thing is unique.
+        </p>  
+      </div>
     </div>
   );
 }
